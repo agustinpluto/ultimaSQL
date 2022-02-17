dccab40cc73cf2ec995baa2a55fa0caddaac39b3
Agustin Pluto <agustinpluto@gmail.com>
2022-02-17 14:17:15 -0300

    listo el login

--- app.js
+++ app.js
@@ -34,10 +34,7 @@ app.use(session({
 
 secured = async (req, res, next) => {
   try {
-
-    console.log('---------------------')
     console.log(req.session.id_usuario);
-    console.log('---------------------')
 
     if (req.session.id_usuario) {
       next();
